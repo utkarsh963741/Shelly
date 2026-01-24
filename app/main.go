98115fd88926a517db4f526323543d120cbfdedd
Utkarsh Singh <utkarsh.singh27800@gmail.com>
2026-01-24 16:15:06 +0530

    implemented type command to check if argument is shell builtin or executable or invalid

--- app/main.go
+++ app/main.go
@@ -8,6 +8,28 @@ import (
 	"strings"
 )
 
+var PATH string = os.Getenv("PATH")
+
+func checkIfExecutable(command string) (bool, string) {
+	paths := strings.Split(PATH, ":")
+	for _, dir := range paths {
+		filePath := fmt.Sprintf("%s/%s", dir, command)
+		if info, err := os.Stat(filePath); err == nil {
+
+			const (
+				ExecAny os.FileMode = 0111 // Any execute permission
+			)
+
+			mode := info.Mode().Perm()
+
+			if mode&ExecAny != 0 {
+				return true, filePath
+			}
+		}
+	}
+	return false, ""
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	commands := []string{"echo", "exit", "type"}
@@ -34,7 +56,11 @@ inputLoop:
 			if slices.Contains(commands, test) {
 				fmt.Printf("%s is a shell builtin\n", test)
 			} else {
-				fmt.Printf("%s: not found\n", test)
+				if found, path := checkIfExecutable(test); found {
+					fmt.Printf("%s is %s\n", test, path)
+				} else {
+					fmt.Printf("%s: not found\n", test)
+				}
 			}
 		default:
 			fmt.Printf("%s: command not found\n", command)
